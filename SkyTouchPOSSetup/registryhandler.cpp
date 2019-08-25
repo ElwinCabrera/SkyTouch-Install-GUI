d31bd882a344c9f51c33f27401e058f89a644f59
Elwin Cabrera <elwincab0@gmail.com>
2019-08-25 11:31:05 -0400

    organized a little more all the policies to add them to the correct group later

--- SkyTouchPOSSetup/registryhandler.cpp
+++ SkyTouchPOSSetup/registryhandler.cpp
@@ -17,42 +17,63 @@ QString RegistryHandler::getCurrRegDataVal(QString policyName){
 }
 
 void RegistryHandler::setDefaultValues(){
-    systemSettings.setValue("DisableCMD", 0); //Prevent access to the command prompt
-    policyNameToType.insert("DisableCMD","SYSTEM");
 
 
-    runSetting.setValue("CashRegisterExpressRun", "C:\\Program Files (x86)\\CRE.NET\\CRE2004"); //
-    policyNameToType.insert("CashRegisterExpressRun", "RUN");
 
-    policySettings.setValue("NoControlPanel", 0); //Prohibit access to Control Panel and PC Settings
-    policySettings.setValue("HidePowerOptions", 0); // Remove and prevent access to the Shut Down, Restart, Sleep, and Hibernate commands
-    policySettings.setValue("Nologoff", 0); // Remove logoff
-    policySettings.setValue("DisabletaskMgr", 0); // Remove Task Manager
-    policySettings.setValue("DisablelockWorkstation", 0); // Remove Lock Computer
-    policySettings.setValue("Disablechangepassword", 0); // Remove Change Passoword
-    policySettings.setValue("NoAutoUpdate", 0); // Windows automatic updates
-    policySettings.setValue("TaskbarNoNotification", 0); // Turn off all balloon notifications
-    policySettings.setValue("NoballoonFeatureAdvertisements", 0);
-
-
+    /************  HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\... ***********/
+    // Explorer
+    policySettings.setValue("NoControlPanel", 1); //Prohibit access to Control Panel and PC Settings
+    policySettings.setValue("HidePowerOptions", 1); // Remove and prevent access to the Shut Down, Restart, Sleep, and Hibernate commands
+    policySettings.setValue("Nologoff", 1); // Remove logoff
+    policySettings.setValue("NoAutoUpdate", 1); // Windows automatic updates
+    policySettings.setValue("TaskbarNoNotification", 1); // Turn off all balloon notifications
     policySettings.setValue("NoChangestartMenu", 0); // Prevent users from customizing their start screen
-    policySettings.setValue("LockTaskbar", 0); // Lock the taskBar
+    policySettings.setValue("LockTaskbar", 1); // Lock the taskBar
     policySettings.setValue("TaskbarNoAddRemoveToolbar", 0); // Prevent users from adding or removing toolbars
     policySettings.setValue("NoSetTaskbar", 0); //Prevent changes to Taskbar and start menu Settings
     policySettings.setValue("taskbarLockAll", 0); // Lock all taskbar settings
-
-    //policySettings.setValue("NoTileApplicationNotification", 0); // Turn off tile notifications -- NOT IN THIS SUBKEY CHANGE LATER
-    //policySettings.setValue("NoCloudApplicationNotification", 0); // Turn off notifications network usage -- NOT IN THIS SUBKEY CHANGE LATER
-    //policySettings.setValue("NoWindowMinimizingShortcuts", 0); // Turn off Aero Shake window minimizing mouse gesture -- NOT IN THIS SUBKEY CHANGE LATER
-    //policySettings.setValue("NoChanginglockscreen", 0); //Prevent changing lock screen and logon image -- NOT IN THIS SUBKEY CHANGE LATER
-    //policySettings.setValue("Nolockscreen", 0); // Do not display the lock screen -- NOT IN THIS SUBKEY CHANGE LATER
-    //policySettings.setValue("NoChangingWallPaper", 0); // Prevent changing desktop background/wallpaper -- NOT IN THIS SUBKEY CHANGE LATER
-
-
     policySettings.setValue("NoMovingBands",0); // Prohibit adjusting desktop toolbars
 
+    // System
+    policySettings.setValue("DisabletaskMgr", 1); // Remove Task Manager
+    policySettings.setValue("DisablelockWorkstation", 1); // Remove Lock Computer
+    policySettings.setValue("Disablechangepassword", 1); // Remove Change Passoword
+
+    /**************** HKCU\Software\Policies\Microsoft\Windows\...  ********/
+    // Explorer
+    policySettings.setValue("NoballoonFeatureAdvertisements", 1);
+
+    // System
+    systemSettings.setValue("DisableCMD", 1); //Prevent access to the command prompt
+    policyNameToType.insert("DisableCMD","SYSTEM");
+
+    //ActiveDesktop
+    policySettings.setValue("NoChangingWallPaper", 1); // Prevent changing desktop background/wallpaper
+
+
+
+    /********************** HKCU\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications\... *******/
+    policySettings.setValue("NoTileApplicationNotification", 1); // Turn off tile notifications
+    policySettings.setValue("NoCloudApplicationNotification", 1); // Turn off notifications network usage
+
+    /***************************** HKCU\Software\Policies\Microsoft\Windows\Explorer\ ... **********************/
+    policySettings.setValue("NoWindowMinimizingShortcuts", 1); // Turn off Aero Shake window minimizing mouse gesture
+
+
+
+    /********************* HKLM\Software\Policies\Microsoft\Windows\Personalization\ ... ************************/
+    policySettings.setValue("NoChanginglockscreen", 0); //Prevent changing lock screen and logon image
+
+    /********************* HKLM\Software\Policies\Microsoft\Windows\Personalization ****************************/
+    policySettings.setValue("Nolockscreen", 1); // Do not display the lock screen
+
+
+
+    /************************ HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer ******************/
     /*
-     * Note: The default account picture is stored at %PROGRAMDATA%\Microsoft\User Account pictures\user.jpg. The default guest picture is stored at %PROGRAMDATA%\Microsoft\User Account pictures\guest.jpg. If the default pictures do not exist, an empty frame is displayed.
+     * Note: The default account picture is stored at %PROGRAMDATA%\Microsoft\User Account pictures\user.jpg.
+     * The default guest picture is stored at %PROGRAMDATA%\Microsoft\User Account pictures\guest.jpg.
+     * If the default pictures do not exist, an empty frame is displayed.
      */
     policySettings.setValue("UseDefaultTile", 0); // Apply the default account picture to all users
 }
