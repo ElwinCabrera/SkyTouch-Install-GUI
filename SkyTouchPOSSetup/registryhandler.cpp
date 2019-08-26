0330a1b7978eb23fbdb67dc24d68d9e674618f55
Elwin Cabrera <elwincab0@gmail.com>
2019-08-26 18:09:51 -0400

    done with this bug all I had to do was to check it the key existed before i set the value. But because of this I have to create a new function to reset the actual values when the reset button is clicked.

--- SkyTouchPOSSetup/registryhandler.cpp
+++ SkyTouchPOSSetup/registryhandler.cpp
@@ -76,55 +76,98 @@ void RegistryHandler::setDefaultValues(){
     /************  HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\... ***********/
     // Explorer
     QSettings policyExplorerSettings(hkcuPolicyKey+"\\Explorer", QSettings::NativeFormat);
-    policyExplorerSettings.setValue("NoControlPanel", 0); //Prohibit access to Control Panel and PC Settings
-    policyExplorerSettings.setValue("HidePowerOptions", 0); // Remove and prevent access to the Shut Down, Restart, Sleep, and Hibernate commands
-    policyExplorerSettings.setValue("NoLogoff", 0); // Remove logoff
-    policyExplorerSettings.setValue("NoAutoUpdate", 0); // Windows automatic updates
-    policyExplorerSettings.setValue("TaskbarNoNotification", 0); // Turn off all balloon notifications
-    policyExplorerSettings.setValue("NoChangestartMenu", 0); // Prevent users from customizing their start screen
-    policyExplorerSettings.setValue("LockTaskbar", 0); // Lock the taskBar
-    policyExplorerSettings.setValue("TaskbarNoAddRemoveToolbar", 0); // Prevent users from adding or removing toolbars
-    policyExplorerSettings.setValue("NoSetTaskbar", 0); //Prevent changes to Taskbar and start menu Settings
-    policyExplorerSettings.setValue("taskbarLockAll", 0); // Lock all taskbar settings
-    policyExplorerSettings.setValue("NoMovingBands",0); // Prohibit adjusting desktop toolbars
+    if(!policyExplorerSettings.childKeys().contains("NoControlPanel", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoControlPanel", 0); //Prohibit access to Control Panel and PC Settings
+
+    if(!policyExplorerSettings.childKeys().contains("HidePowerOptions", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("HidePowerOptions", 0); // Remove and prevent access to the Shut Down, Restart, Sleep, and Hibernate commands
+
+    if(!policyExplorerSettings.childKeys().contains("NoLogoff", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoLogoff", 0); // Remove logoff
+
+    if(!policyExplorerSettings.childKeys().contains("NoAutoUpdate", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoAutoUpdate", 0); // Windows automatic updates
+
+    if(!policyExplorerSettings.childKeys().contains("TaskbarNoNotification", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("TaskbarNoNotification", 0); // Turn off all balloon notifications
+
+    if(!policyExplorerSettings.childKeys().contains("NoChangestartMenu", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoChangestartMenu", 0); // Prevent users from customizing their start screen
+
+    if(!policyExplorerSettings.childKeys().contains("LockTaskbar", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("LockTaskbar", 0); // Lock the taskBar
+
+    if(!policyExplorerSettings.childKeys().contains("TaskbarNoAddRemoveToolbar", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("TaskbarNoAddRemoveToolbar", 0); // Prevent users from adding or removing toolbars
+
+    if(!policyExplorerSettings.childKeys().contains("NoSetTaskbar", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoSetTaskbar", 0); //Prevent changes to Taskbar and start menu Settings
+
+    if(!policyExplorerSettings.childKeys().contains("taskbarLockAll", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("taskbarLockAll", 0); // Lock all taskbar settings
+
+    if(!policyExplorerSettings.childKeys().contains("NoMovingBands", Qt::CaseSensitive))
+        policyExplorerSettings.setValue("NoMovingBands",0); // Prohibit adjusting desktop toolbars
 
 
     // System
     QSettings policySystemSettings(hkcuPolicyKey+"\\System", QSettings::NativeFormat);
-    policySystemSettings.setValue("DisabletaskMgr", 0); // Remove Task Manager
-    policySystemSettings.setValue("DisablelockWorkstation", 0); // Remove Lock Computer
-    policySystemSettings.setValue("Disablechangepassword", 0); // Remove Change Passoword
+
+    if(!policySystemSettings.childKeys().contains("DisabletaskMgr", Qt::CaseSensitive))
+        policySystemSettings.setValue("DisabletaskMgr", 0); // Remove Task Manager
+
+    if(!policySystemSettings.childKeys().contains("DisablelockWorkstation", Qt::CaseSensitive))
+        policySystemSettings.setValue("DisablelockWorkstation", 0); // Remove Lock Computer
+
+    if(!policySystemSettings.childKeys().contains("Disablechangepassword", Qt::CaseSensitive))
+        policySystemSettings.setValue("Disablechangepassword", 0); // Remove Change Passoword
 
 
     /**************** HKCU\Software\Policies\Microsoft\Windows\...  ********/
     // Explorer
     QSettings policyWinExplorerSettings(hkcuPolicyWinKey+"\\Explorer", QSettings::NativeFormat);
-    policyWinExplorerSettings.setValue("NoballoonFeatureAdvertisements", 0);
-    policyWinExplorerSettings.setValue("NoWindowMinimizingShortcuts", 0); // Turn off Aero Shake window minimizing mouse gesture
+
+    if(!policyWinExplorerSettings.childKeys().contains("NoballoonFeatureAdvertisements", Qt::CaseSensitive))
+        policyWinExplorerSettings.setValue("NoballoonFeatureAdvertisements", 0);
+
+    if(!policyWinExplorerSettings.childKeys().contains("NoWindowMinimizingShortcuts", Qt::CaseSensitive))
+        policyWinExplorerSettings.setValue("NoWindowMinimizingShortcuts", 0); // Turn off Aero Shake window minimizing mouse gesture
 
 
     // System
     QSettings policyWinSystemSettings(hkcuPolicyWinKey+"\\System", QSettings::NativeFormat);
-    policyWinSystemSettings.setValue("DisableCMD", 0); //Prevent access to the command prompt
+
+    if(!policyWinSystemSettings.childKeys().contains("DisableCMD", Qt::CaseSensitive))
+        policyWinSystemSettings.setValue("DisableCMD", 0); //Prevent access to the command prompt
 
 
     //ActiveDesktop
     QSettings policyWinActiveDesktopSettings(hkcuPolicyWinKey+"\\ActiveDesktop", QSettings::NativeFormat);
-    policyWinActiveDesktopSettings.setValue("NoChangingWallPaper", 0); // Prevent changing desktop background/wallpaper
+
+    if(!policyWinActiveDesktopSettings.childKeys().contains("NoChangingWallPaper", Qt::CaseSensitive))
+        policyWinActiveDesktopSettings.setValue("NoChangingWallPaper", 0); // Prevent changing desktop background/wallpaper
 
 
 
     /********************** HKCU\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications\... *******/
     QSettings policyPushNotiSettings(hkcuPolicyPushNotiKey, QSettings::NativeFormat);
-    policyPushNotiSettings.setValue("NoTileApplicationNotification", 0); // Turn off tile notifications
-    policyPushNotiSettings.setValue("NoCloudApplicationNotification", 0); // Turn off notifications network usage
+
+    if(!policyPushNotiSettings.childKeys().contains("NoTileApplicationNotification", Qt::CaseSensitive))
+        policyPushNotiSettings.setValue("NoTileApplicationNotification", 0); // Turn off tile notifications
+
+    if(!policyPushNotiSettings.childKeys().contains("NoCloudApplicationNotification", Qt::CaseSensitive))
+        policyPushNotiSettings.setValue("NoCloudApplicationNotification", 0); // Turn off notifications network usage
 
 
 
     /********************* HKLM\Software\Policies\Microsoft\Windows\Personalization\ ... ************************/
     QSettings policyPersonalSettings(hklmPolicyPersonalKey, QSettings::NativeFormat);
-    policyPersonalSettings.setValue("NoChanginglockscreen", 0); //Prevent changing lock screen and logon image
-    policyPersonalSettings.setValue("Nolockscreen", 0); // Do not display the lock screen
+
+    if(!policyPersonalSettings.childKeys().contains("NoChanginglockscreen", Qt::CaseSensitive))
+        policyPersonalSettings.setValue("NoChanginglockscreen", 0); //Prevent changing lock screen and logon image
+
+    if(!policyPersonalSettings.childKeys().contains("Nolockscreen", Qt::CaseSensitive))
+        policyPersonalSettings.setValue("Nolockscreen", 0); // Do not display the lock screen
 
 
     /************************ HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer ******************/
@@ -135,5 +178,6 @@ void RegistryHandler::setDefaultValues(){
      * The default guest picture is stored at %PROGRAMDATA%\Microsoft\User Account pictures\guest.jpg.
      * If the default pictures do not exist, an empty frame is displayed.
      */
-    policyExplorerSettingsHKLM.setValue("UseDefaultTile", 0); // Apply the default account picture to all users
+    if(!policyExplorerSettingsHKLM.childKeys().contains("UseDefaultTile", Qt::CaseSensitive))
+        policyExplorerSettingsHKLM.setValue("UseDefaultTile", 0); // Apply the default account picture to all users
 }
